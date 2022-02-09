1e52c9e81a5803553c5a4cdb7c41f1bf70f202bb
Michael Gosling <mgosling94@gmail.com>
2022-02-09 15:04:50 -0400

    Play a sound when a drum pad button is clicked

--- DrumPad/MainWindow.xaml.cs
+++ DrumPad/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.Media;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,8 +22,25 @@ namespace DrumPad {
   /// An empty window that can be used on its own or navigated to within a Frame.
   /// </summary>
   public sealed partial class MainWindow : Window {
+
     public MainWindow() {
       this.InitializeComponent();
+    }
+
+    private void findAndPlayWavAsset(string wavName) {
+      // get the full path to your app's folder where it is installed
+      var installedPath = Windows.ApplicationModel.Package.Current.InstalledLocation.Path;
+      // join path above with the sub-paths in your Assets folder and the specific sound file
+      var soundFile = Path.Join(installedPath, "Assets", $"{wavName}.wav");
+
+      SoundPlayer player = new System.Media.SoundPlayer(soundFile);
+      player.Play();
+    }
+
+    private void pad_Click(object sender, RoutedEventArgs e) {
+      var button = (Button) sender;
+      var soundName = button.Tag.ToString();
+      findAndPlayWavAsset(soundName);
     }
   }
 }
